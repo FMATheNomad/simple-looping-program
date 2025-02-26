f6830a8566715221f629ec29491d9912fba7c376
FMATheNomad <111466154+FMATheNomad@users.noreply.github.com>
2025-02-26 20:58:41 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function arYuGey() {
+    let diterima = false;
+    while (!diterima) {
+        diterima = confirm("Are you Gay? (OK for Yes, Cancel for No)");
+    }
+    alert("You are da most gayest person in da world");
+}
+
+arYuGey();
